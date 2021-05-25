7dd48eceba715ab1b291a0884d24ea11fef70cb7
jojo2829 <joulinli@gmail.com>
2021-05-25 00:10:13 +0000

    implement head using lodash

---
+++ head.js
@@ -0,0 +1,19 @@
+// FUNCTION IMPLEMENTATION
+var _ = require('lodash');
+
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    return console.log(`Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    return console.log(`Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+let head = (array) => {
+  return _.head(array);
+};
+
+// TEST CODE
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
