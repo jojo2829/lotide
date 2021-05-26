57282f32c9df92435a337b263fad5311b1a0ede4
jojo2829 <joulinli@gmail.com>
2021-05-26 21:46:58 +0000

    added finding middle numbers within an array function

---
+++ middle.js
@@ -0,0 +1,53 @@
+const eqArrays = function(arrayOne, arrayTwo) {
+  if (arrayOne.length !== arrayTwo.length) {
+    return false;
+  }
+  for (let i = 0; i < arrayOne.length; i++) {
+    if (arrayOne[i] !== arrayTwo[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(arrayA, arrayB) {
+
+  if (eqArrays(arrayA,arrayB)) {
+    console.log(`Assertion Passed: ${arrayA} === ${arrayB}`);
+
+  } else {
+    console.log(`Assertion Failed: ${arrayA} !== ${arrayB}`);
+  }
+};
+
+const middle = function (midArray) {
+  let newArray = [];
+  let i = 0;
+
+  if (midArray.length < 3) {
+    console.log("less than 2:", newArray);
+    return [];
+  }
+  // even number
+  else if (midArray.length % 2 === 0) {
+    i = midArray.length / 2;
+    newArray.push(midArray[i-1],midArray[i]);
+  }
+  // odd number
+  else if (midArray.length % 2 !== 0) {
+    i = midArray.length / 2;
+    newArray.push(midArray[i]);
+  };
+  return newArray;
+  
+
+};
+
+
+  
+middle([1,2])
+middle([1, 2, 3, 4]) // => [2, 3]
+middle([1, 2, 3, 4, 5]) // => [3]
+
+assertArraysEqual(middle([1, 2, 3, 4]), [2,3]);
+assertArraysEqual([1,2,3],[3,2,1]);
