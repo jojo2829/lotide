5b8628372dac15cb0c5b3690274850b67fe23d7c
jojo2829 <joulinli@gmail.com>
2021-05-26 19:55:58 +0000

    eslint

--- eqArray.js
+++ eqArray.js
@@ -6,17 +6,17 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-const eqArrays = function (arrayOne, arrayTwo){
-  if (arrayOne.length !== arrayTwo.length){
+const eqArrays = function(arrayOne, arrayTwo) {
+  if (arrayOne.length !== arrayTwo.length) {
     return false;
   }
-  for (let i = 0; i < arrayOne.length; i++){
-    if (arrayOne[i] !== arrayTwo[i]){
+  for (let i = 0; i < arrayOne.length; i++) {
+    if (arrayOne[i] !== arrayTwo[i]) {
       return false;
     }
   }
   return true;
-}
+};
 
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]),true); // => true
 assertEqual(eqArrays([1, 2, 3], [3, 2, 1]),true); // => false
