a11b895649d04b4d6d63240d680192c34ac69679
jojo2829 <joulinli@gmail.com>
2021-05-26 19:57:21 +0000

    rework and refactor

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,23 +1,22 @@
+const eqArrays = function(arrayOne, arrayTwo) {
+  if (arrayOne.length !== arrayTwo.length) {
+    return false;
+  }
+  for (let i = 0; i < arrayOne.length; i++) {
+    if (arrayOne[i] !== arrayTwo[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
 const assertArraysEqual = function(arrayA, arrayB) {
 
-  if (arrayA.length === arrayB.length) {
-    for (let i = 0; i < arrayA.length; i++) {
-      let itemA = arrayA[i];
-      for (let j = 0; j < arrayB.length; j++) {
-        let itemB = arrayB[j];
-        if (i === j) {
-          if (itemA !== itemB) {
-            console.log(`Assertion Failed: ${arrayA} !== ${arrayB}`);
-            return;
-          }
-        }
-      }
-    }
+  if (eqArrays(arrayA,arrayB)) {
     console.log(`Assertion Passed: ${arrayA} === ${arrayB}`);
-    return;
+
   } else {
     console.log(`Assertion Failed: ${arrayA} !== ${arrayB}`);
-    return;
   }
 };
 
