2a9259dfb81b4cbeaf74dc82beea7d913b3d7f9a
jojo2829 <joulinli@gmail.com>
2021-05-27 01:44:41 +0000

    count letters within a string

---
+++ countLetters.js
@@ -0,0 +1,17 @@
+const countLetters = function(string) {
+  const result = {};
+  
+  for (let char of string) {
+
+    if (char !== " ") {
+      if (result[char]) {
+        result[char] += 1;
+      } else {
+        result[char] = 1;
+      }
+    }
+  }
+  return result;
+};
+
+console.log(countLetters("lighthouse in the house"));
