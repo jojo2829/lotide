5daaede598dce9a858a9362b0a5ef2db3e020e09
jojo2829 <joulinli@gmail.com>
2021-05-27 05:02:44 +0000

    loop through array

--- eqObjects.js
+++ eqObjects.js
@@ -24,7 +24,7 @@ const eqObjects = function(object1, object2) {
 
     for (let key in object1) {
       if (object2.hasOwnProperty(key)) {
-        if (object1[key].length > 0 && object2[key].length > 0 && object1[key].length === object2[key].length) {
+        if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
           if (eqArrays(object1[key], object2[key]) === false)
             return false;
         } else {
@@ -49,9 +49,9 @@ console.log(eqObjects(cd, dc)); // => true
 const cd2 = { c: "1", d: ["2", 3, 4] };
 console.log(eqObjects(cd, cd2)); // => false
 
-// const ab = { a: "1", b: "2" };
-// const ba = { b: "2", a: "1" };
-// console.log(assertEqual(eqObjects(ab, ba), true)); // => true
+const ab = { a: "1", b: "2" };
+const ba = { b: "2", a: "1" };
+console.log(assertEqual(eqObjects(ab, ba), true)); // => true
 
-// const abc = { a: "1", b: "2", c: "3" };
-// console.log(assertEqual(eqObjects(ab, abc), true)); // => false
+const abc = { a: "1", b: "2", c: "3" };
+console.log(assertEqual(eqObjects(ab, abc), true)); // => false
