7fdd93b91a330426214f1a09c68c2a99c4dc0a89
jojo2829 <joulinli@gmail.com>
2021-05-27 02:28:45 +0000

    array of numbers to represent the positions of each occurance

---
+++ letterPositions.js
@@ -0,0 +1,41 @@
+const eqArrays = function(arrayOne, arrayTwo) {
+  if (arrayOne.length !== arrayTwo.length) {
+    return false;
+  }
+  for (let i = 0; i < arrayOne.length; i++) {
+    if (arrayOne[i] !== arrayTwo[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(arrayA, arrayB) {
+
+  if (eqArrays(arrayA,arrayB)) {
+    console.log(`Assertion Passed: ${arrayA} === ${arrayB}`);
+
+  } else {
+    console.log(`Assertion Failed: ${arrayA} !== ${arrayB}`);
+  }
+};
+
+const letterPositions = function(sentence) {
+  const results = {};
+
+  for (let i = 0; i < sentence.length; i++) {
+    let letter = sentence[i];
+
+    if (letter !== " ") {
+      
+      if (!results[letter])
+        results[letter] = [];
+
+      results[letter].push(i);
+    }
+  }
+  return results;
+};
+
+console.log(letterPositions("lighthouse in the house"));
+console.log(assertArraysEqual(letterPositions("hello").e, [1]));
