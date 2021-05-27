ce5ae35e5a4926be9ae69d9e160e57615ebf899f
jojo2829 <joulinli@gmail.com>
2021-05-27 19:14:52 +0000

    update console log format

--- assertEqual.js
+++ assertEqual.js
@@ -1,9 +1,9 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    return console.log("Assertion Passed: " + actual + " === " + expected);
+    return console.log(`Assertion Passed: ${actual} === ${expected}`);
   } else if (actual !== expected) {
-    return console.log("Assertion Failed: " + actual + " !== " + expected);
+    return console.log(`Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
