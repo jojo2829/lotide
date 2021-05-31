458ed0f98b246cd63c019707f534d01f185acadc
jojo2829 <joulinli@gmail.com>
2021-05-31 20:23:40 +0000

    eqArrayTest complete

---
+++ test/eqArrayTest.js
@@ -0,0 +1,8 @@
+const eqArrays = require("../eqArray")
+const assertEqual = require('../assertEqual');
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]),true); // => true
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]),true); // => false
+
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]),true); // => true
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]),true); // => false
