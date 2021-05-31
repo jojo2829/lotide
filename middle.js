6b2fbda06f5107a0adb77313549c2dd372501b71
jojo2829 <joulinli@gmail.com>
2021-05-31 21:46:49 +0000

    refactor middle

--- middle.js
+++ middle.js
@@ -1,24 +1,4 @@
-const eqArrays = function(arrayOne, arrayTwo) {
-  if (arrayOne.length !== arrayTwo.length) {
-    return false;
-  }
-  for (let i = 0; i < arrayOne.length; i++) {
-    if (arrayOne[i] !== arrayTwo[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const assertArraysEqual = function(arrayA, arrayB) {
-
-  if (eqArrays(arrayA,arrayB)) {
-    console.log(`Assertion Passed: ${arrayA} === ${arrayB}`);
-
-  } else {
-    console.log(`Assertion Failed: ${arrayA} !== ${arrayB}`);
-  }
-};
+const assertArraysEqual = require("./assertArraysEqual");
 
 const middle = function(midArray) {
   let newArray = [];
@@ -26,21 +6,24 @@ const middle = function(midArray) {
 
   if (midArray.length < 3) {
     return [];
-  } else if (midArray.length % 2 === 0) {
+  }
+  if (midArray.length % 2 === 0) {
     i = midArray.length / 2;
     newArray.push(midArray[i - 1],midArray[i]);
-  } else if (midArray.length % 2 !== 0) {
+  }
+  if (midArray.length % 2 === 1) {
     i = midArray.length / 2;
-    newArray.push(midArray[i]);
+    newArray.push(midArray[i - 0.5]);
   }
   return newArray;
 };
 
+module.exports = middle;
 
   
-middle([1,2]);
-middle([1, 2, 3, 4]); // => [2, 3]
-middle([1, 2, 3, 4, 5]); // => [3]
+// middle([1,2]);
+// middle([1, 2, 3, 4]); // => [2, 3]
+// middle([1, 2, 3, 4, 5]); // => [3]
 
-assertArraysEqual(middle([1, 2, 3, 4]), [2,3]);
-assertArraysEqual([1,2,3],[3,2,1]);
+// assertArraysEqual(middle([1, 2, 3, 4]), [2,3]);
+// assertArraysEqual([1,2,3],[3,2,1]);
