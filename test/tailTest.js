17516a41c4a408aa92ef8caa51de8dc026103ba3
jojo2829 <joulinli@gmail.com>
2021-05-31 22:40:47 +0000

    test using mocha chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,6 +1,21 @@
 const tail = require('../tail');
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-assertEqual(words.length, 3); // original array should still have 3 elements!
-assertEqual(tail(words), 3); // original array should still have 3 elements!
+describe("#tail", () => {
+  it("returns 2 for length of[\"Yo Yo\", \"Lighthouse\", \"Labs\"] ", () => {
+    const words = ["Yo Yo", "Lighthouse", "Labs"];
+    const result = tail(words);
+    assert.strictEqual(result.length, 2); 
+  });
+
+  it("returns \"Labs\" for 2nd item of[\"Yo Yo\", \"Lighthouse\", \"Labs\"] ", () => {
+    const words = ["Yo Yo", "Lighthouse", "Labs"];
+    const result = tail(words);
+    assert.strictEqual(result[1], "Labs"); 
+  });
+});
+
+
+// const words = ["Yo Yo", "Lighthouse", "Labs"];
+// assertEqual(words.length, 3); // original array should still have 3 elements!
+// assertEqual(tail(words), 3); // original array should still have 3 elements!
