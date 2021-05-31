267b971f2308f2683748d8384d0082601b79a5a8
jojo2829 <joulinli@gmail.com>
2021-05-31 23:12:03 +0000

    implemented mocha chai

--- test/assertEqualTest.js
+++ test/assertEqualTest.js
@@ -1,4 +1,15 @@
 const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+describe("#assertEqual", () => {
+  it("returns false, Lighthouse Labs !== Bootcamp", () => {
+    assert.strictEqual(assertEqual("Lighthouse Labs", "Bootcamp"));
+  });
+
+  it("returns true, 1 === 1", () => {
+    assert.strictEqual(assertEqual(1, 1)); 
+  });
+});
+
+// assertEqual("Lighthouse Labs", "Bootcamp");
+// assertEqual(1, 1);
