b5d628b1caf68ba5b2e437231bbb07fb016a4d45
jojo2829 <joulinli@gmail.com>
2021-05-31 20:14:14 +0000

    refactor tail

--- tail.js
+++ tail.js
@@ -1,23 +1,14 @@
 // // FUNCTION IMPLEMENTATION
-// let _ = require('lodash');
-
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    return console.log(`Assertion Passed: ${actual} === ${expected}`);
-  } else if (actual !== expected) {
-    return console.log(`Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const tail = function(words) {
   let newArray = [];
   newArray = words.slice(1);
   return newArray;
 };
 
+module.exports = tail;
 
 // TEST CODE
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-console.log(tail(words)); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
-assertEqual(tail(words), 3); // original array should still have 3 elements!
+// const words = ["Yo Yo", "Lighthouse", "Labs"];
+// console.log(tail(words)); // no need to capture the return value since we are not checking it
+// assertEqual(words.length, 3); // original array should still have 3 elements!
+// assertEqual(tail(words), 3); // original array should still have 3 elements!
