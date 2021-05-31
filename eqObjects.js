bacddf62e977d1fd0ccdc786aab2df36b08d0668
jojo2829 <joulinli@gmail.com>
2021-05-31 23:37:44 +0000

    added module export

--- eqObjects.js
+++ eqObjects.js
@@ -52,16 +52,18 @@ const eqObjects = function(object1, object2) {
   }
 };
 
-const cd = { c: "1", d: ["2", 3] };
-const dc = { d: ["2", 3], c: "1" };
-console.log(eqObjects(cd, dc)); // => true
+module.exports = eqObjects;
 
-const cd2 = { c: "1", d: ["2", 3, 4] };
-console.log(eqObjects(cd, cd2)); // => false
+// const cd = { c: "1", d: ["2", 3] };
+// const dc = { d: ["2", 3], c: "1" };
+// console.log(eqObjects(cd, dc)); // => true
 
-const ab = { a: "1", b: "2" };
-const ba = { b: "2", a: "1" };
-console.log(assertEqual(eqObjects(ab, ba), true)); // => true
+// const cd2 = { c: "1", d: ["2", 3, 4] };
+// console.log(eqObjects(cd, cd2)); // => false
 
-const abc = { a: "1", b: "2", c: "3" };
-console.log(assertEqual(eqObjects(ab, abc), true)); // => false
+// const ab = { a: "1", b: "2" };
+// const ba = { b: "2", a: "1" };
+// console.log(assertEqual(eqObjects(ab, ba), true)); // => true
+
+// const abc = { a: "1", b: "2", c: "3" };
+// console.log(assertEqual(eqObjects(ab, abc), true)); // => false
