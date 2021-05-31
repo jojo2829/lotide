6d1e56b372bc6b2d5bf0842673c81a03e3d434ab
jojo2829 <joulinli@gmail.com>
2021-05-31 19:38:52 +0000

    refactor assertEqual and head, removed test codes

--- head.js
+++ head.js
@@ -1,20 +1,10 @@
 // FUNCTION IMPLEMENTATION
-// let _ = require('lodash');
-
-const assertEqual = function(actual, expected) {
-
-  if (actual === expected) {
-    return console.log(`Assertion Passed: ${actual} === ${expected}`);
-  } else if (actual !== expected) {
-    return console.log(`Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const head = function(array) {
   return array[0];
 };
 
-
 // TEST CODE
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+// assertEqual(head([5,6,7]), 5);
+// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+
+module.exports = head;
